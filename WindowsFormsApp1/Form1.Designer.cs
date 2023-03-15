0b35efb38eb8912db2c4ba61f34edbbc3df4d92f
MOHAMMED EL ASRI <ELASRI.MOHAMMED8@ofppt-edu.ma>
2023-03-15 21:58:08 +0100

    Ajoutez des fichiers projet.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DV = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prenom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbNom = new System.Windows.Forms.TextBox();
+            this.tbAge = new System.Windows.Forms.TextBox();
+            this.tbPrenom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bt_AjouterList = new System.Windows.Forms.Button();
+            this.btAjouter = new System.Windows.Forms.Button();
+            this.bt_Supprimer = new System.Windows.Forms.Button();
+            this.bt_Modifier = new System.Windows.Forms.Button();
+            this.bt_Nouveau = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DV
+            // 
+            this.DV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DV.BackgroundColor = System.Drawing.Color.White;
+            this.DV.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.RosyBrown;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DV.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.DV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nom,
+            this.Prenom,
+            this.Age});
+            this.DV.Location = new System.Drawing.Point(6, 215);
+            this.DV.Margin = new System.Windows.Forms.Padding(2);
+            this.DV.Name = "DV";
+            this.DV.ReadOnly = true;
+            this.DV.RowHeadersWidth = 82;
+            this.DV.RowTemplate.Height = 33;
+            this.DV.Size = new System.Drawing.Size(714, 293);
+            this.DV.TabIndex = 4;
+            this.DV.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DV_CellClick);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 10;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Nom
+            // 
+            this.Nom.HeaderText = "Nom";
+            this.Nom.MinimumWidth = 10;
+            this.Nom.Name = "Nom";
+            this.Nom.ReadOnly = true;
+            // 
+            // Prenom
+            // 
+            this.Prenom.HeaderText = "Prenom";
+            this.Prenom.MinimumWidth = 10;
+            this.Prenom.Name = "Prenom";
+            this.Prenom.ReadOnly = true;
+            // 
+            // Age
+            // 
+            this.Age.HeaderText = "Age";
+            this.Age.MinimumWidth = 10;
+            this.Age.Name = "Age";
+            this.Age.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 199);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Liste Etu";
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(84, 12);
+            this.tbID.Margin = new System.Windows.Forms.Padding(2);
+            this.tbID.Multiline = true;
+            this.tbID.Name = "tbID";
+            this.tbID.ReadOnly = true;
+            this.tbID.Size = new System.Drawing.Size(106, 28);
+            this.tbID.TabIndex = 2;
+            // 
+            // tbNom
+            // 
+            this.tbNom.Location = new System.Drawing.Point(84, 48);
+            this.tbNom.Margin = new System.Windows.Forms.Padding(2);
+            this.tbNom.Multiline = true;
+            this.tbNom.Name = "tbNom";
+            this.tbNom.Size = new System.Drawing.Size(106, 28);
+            this.tbNom.TabIndex = 0;
+            // 
+            // tbAge
+            // 
+            this.tbAge.Location = new System.Drawing.Point(84, 119);
+            this.tbAge.Margin = new System.Windows.Forms.Padding(2);
+            this.tbAge.Multiline = true;
+            this.tbAge.Name = "tbAge";
+            this.tbAge.Size = new System.Drawing.Size(106, 28);
+            this.tbAge.TabIndex = 2;
+            // 
+            // tbPrenom
+            // 
+            this.tbPrenom.Location = new System.Drawing.Point(84, 83);
+            this.tbPrenom.Margin = new System.Windows.Forms.Padding(2);
+            this.tbPrenom.Multiline = true;
+            this.tbPrenom.Name = "tbPrenom";
+            this.tbPrenom.Size = new System.Drawing.Size(106, 28);
+            this.tbPrenom.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 53);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nom";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 125);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Age";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 89);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Prenom";
+            // 
+            // bt_AjouterList
+            // 
+            this.bt_AjouterList.Location = new System.Drawing.Point(552, 18);
+            this.bt_AjouterList.Margin = new System.Windows.Forms.Padding(2);
+            this.bt_AjouterList.Name = "bt_AjouterList";
+            this.bt_AjouterList.Size = new System.Drawing.Size(82, 38);
+            this.bt_AjouterList.TabIndex = 10;
+            this.bt_AjouterList.Text = "Ajoute Liste";
+            this.bt_AjouterList.UseVisualStyleBackColor = true;
+            this.bt_AjouterList.Visible = false;
+            this.bt_AjouterList.Click += new System.EventHandler(this.bt_AjouterList_Click);
+            // 
+            // btAjouter
+            // 
+            this.btAjouter.Location = new System.Drawing.Point(638, 59);
+            this.btAjouter.Margin = new System.Windows.Forms.Padding(2);
+            this.btAjouter.Name = "btAjouter";
+            this.btAjouter.Size = new System.Drawing.Size(82, 38);
+            this.btAjouter.TabIndex = 3;
+            this.btAjouter.Text = "Ajouter";
+            this.btAjouter.UseVisualStyleBackColor = true;
+            this.btAjouter.Click += new System.EventHandler(this.Ajouter_Click);
+            // 
+            // bt_Supprimer
+            // 
+            this.bt_Supprimer.Location = new System.Drawing.Point(638, 143);
+            this.bt_Supprimer.Margin = new System.Windows.Forms.Padding(2);
+            this.bt_Supprimer.Name = "bt_Supprimer";
+            this.bt_Supprimer.Size = new System.Drawing.Size(82, 38);
+            this.bt_Supprimer.TabIndex = 12;
+            this.bt_Supprimer.Text = "Supprimer";
+            this.bt_Supprimer.UseVisualStyleBackColor = true;
+            this.bt_Supprimer.Click += new System.EventHandler(this.bt_Supprimer_Click);
+            // 
+            // bt_Modifier
+            // 
+            this.bt_Modifier.Location = new System.Drawing.Point(638, 101);
+            this.bt_Modifier.Margin = new System.Windows.Forms.Padding(2);
+            this.bt_Modifier.Name = "bt_Modifier";
+            this.bt_Modifier.Size = new System.Drawing.Size(82, 38);
+            this.bt_Modifier.TabIndex = 13;
+            this.bt_Modifier.Text = "Modifier";
+            this.bt_Modifier.UseVisualStyleBackColor = true;
+            this.bt_Modifier.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bt_Nouveau
+            // 
+            this.bt_Nouveau.Location = new System.Drawing.Point(638, 17);
+            this.bt_Nouveau.Margin = new System.Windows.Forms.Padding(2);
+            this.bt_Nouveau.Name = "bt_Nouveau";
+            this.bt_Nouveau.Size = new System.Drawing.Size(82, 38);
+            this.bt_Nouveau.TabIndex = 14;
+            this.bt_Nouveau.Text = "Nouveau";
+            this.bt_Nouveau.UseVisualStyleBackColor = true;
+            this.bt_Nouveau.Click += new System.EventHandler(this.bt_Nouveau_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 514);
+            this.Controls.Add(this.bt_Nouveau);
+            this.Controls.Add(this.bt_Modifier);
+            this.Controls.Add(this.bt_Supprimer);
+            this.Controls.Add(this.btAjouter);
+            this.Controls.Add(this.bt_AjouterList);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbAge);
+            this.Controls.Add(this.tbPrenom);
+            this.Controls.Add(this.tbNom);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DV);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Etudiant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Prenom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbNom;
+        private System.Windows.Forms.TextBox tbAge;
+        private System.Windows.Forms.TextBox tbPrenom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button bt_AjouterList;
+        private System.Windows.Forms.Button btAjouter;
+        private System.Windows.Forms.Button bt_Supprimer;
+        private System.Windows.Forms.Button bt_Modifier;
+        private System.Windows.Forms.Button bt_Nouveau;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int position = -1, IDEtu;
+        bool Check = false;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private bool SiVide()
+        {
+            if (this.tbID.Text.Trim().Equals(string.Empty) || this.tbNom.Text.Trim().Equals(string.Empty)
+                || this.tbPrenom.Text.Trim().Equals(string.Empty) || this.tbAge.Text.Trim().Equals(string.Empty))
+                return false;
+            return true;
+        }
+
+        private void EmptyTb()
+        {
+            tbNom.Text = string.Empty;
+            tbPrenom.Text = string.Empty;
+            tbAge.Text = string.Empty;
+
+            int nbrLigne = DV.Rows.Count - 1;
+            tbID.Text = (Convert.ToInt32(DV.Rows[nbrLigne].Cells[0].Value.ToString()) + 1).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DV.AllowUserToAddRows = false;
+            bt_AjouterList_Click(sender, e);
+            EmptyTb();
+            tbNom.Focus();
+        }
+
+        string connaixionstring = ConfigurationManager.ConnectionStrings["CnxSqlServer"].ConnectionString;
+        private void bt_AjouterList_Click(object sender, EventArgs e)
+        {
+            SqlConnection cnx = new SqlConnection();
+            cnx.ConnectionString = connaixionstring;
+
+
+            string query = "Select * from Etudients;";
+            SqlCommand cmd = new SqlCommand(query, cnx);
+
+            cnx.Open();
+            SqlDataReader rd = cmd.ExecuteReader();
+
+
+            if (rd.HasRows)
+            {
+                this.DV.Rows.Clear();
+                while (rd.Read())
+                {
+                    this.DV.Rows.Add(rd[0], rd[1], rd[2], rd[3]);
+                }
+                cnx.Close();
+            }
+            else
+            {
+                MessageBox.Show("la table etudiants est vide !!");
+            }
+
+
+
+            
+        }
+
+        private void Ajouter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Check == true)
+                    return;
+
+                if (!SiVide())
+                {
+                    MessageBox.Show("Veuillez saisir tout les info");
+                    return;
+                }
+
+                using(SqlConnection cnx = new SqlConnection(connaixionstring)) 
+                {
+                    string Query = $"INSERT INTO Etudients VALUES({this.tbID.Text.Trim()},'{this.tbNom.Text.Trim()}','{this.tbPrenom.Text.Trim()}',{this.tbAge.Text.Trim()});";
+                    SqlCommand cmd = new SqlCommand(Query, cnx);
+                    if (cnx.State == ConnectionState.Open)
+                        cnx.Close() ;
+                    cnx.Open();
+
+                    int a = cmd.ExecuteNonQuery();
+                    bt_AjouterList_Click(sender, e);
+
+                    EmptyTb();
+
+                    MessageBox.Show(a + " ligne(s) affectee");
+                    cnx.Close();
+                    
+                    
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message, "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //
+        }
+
+        
+
+        
+        
+
+        private void DV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            position = this.DV.CurrentRow.Index;
+            IDEtu = int.Parse(this.DV.Rows[position].Cells[0].Value.ToString());
+
+            tbID.Text = this.DV.Rows[position].Cells[0].Value.ToString();
+            tbNom.Text = this.DV.Rows[position].Cells[1].Value.ToString();
+            tbPrenom.Text = this.DV.Rows[position].Cells[2].Value.ToString();
+            tbAge.Text = this.DV.Rows[position].Cells[3].Value.ToString();
+
+            tbNom.Enabled = false;
+            tbPrenom.Enabled = false;
+            tbAge.Enabled = false;
+            Check = true;
+        }
+
+        private void bt_Nouveau_Click(object sender, EventArgs e)
+        {
+            EmptyTb();
+            tbNom.Enabled = true;
+            tbPrenom.Enabled = true;
+            tbAge.Enabled = true;
+            tbNom.Focus();
+
+
+        }
+
+        int valid = -1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (position == -1)
+            {
+                MessageBox.Show("Selectionnez un ligne");
+                return;
+            }
+            else if (valid == -1)
+            {
+                bt_Nouveau.Enabled = false;
+                btAjouter.Enabled = false;
+                tbAge.Enabled=true;
+                tbNom.Enabled = true;
+                tbPrenom.Enabled = true;
+                bt_Modifier.Text = "Valider";
+                valid = 1;
+                
+            }
+            else if(valid == 1)
+            {
+
+                if (!SiVide())
+                {
+                    MessageBox.Show("Completez-Vous les Information S'il vous plait");
+                    return;
+                }
+                    
+                
+                string Query = "UPDATE Etudients SET Nom = @p1 , Prenom = @p2 , Age = @p3 WHERE ID = @p4;";
+                using(SqlConnection cnx = new SqlConnection(connaixionstring))
+                {
+                    SqlCommand command = new SqlCommand(Query, cnx);
+                    command.Parameters.AddWithValue("@p1", tbNom.Text.Trim());
+                    command.Parameters.AddWithValue("@p2", tbPrenom.Text.Trim());
+                    command.Parameters.AddWithValue("@p3", tbAge.Text.Trim());
+                    command.Parameters.AddWithValue("@p4", tbID.Text.Trim());
+                    cnx.Open();
+                    command.ExecuteNonQuery();
+
+                    cnx.Close();
+
+                    bt_AjouterList_Click(sender,e);
+                    MessageBox.Show("Le livre est bien Modifier");
+
+                }
+                valid = -1;
+                position = -1;
+                EmptyTb();
+                bt_Modifier.Text = "Modifier";
+                bt_Nouveau.Enabled = true;
+                btAjouter.Enabled = true;
+
+                Check = false;
+
+
+            }
+
+            
+        }
+
+        private void bt_Supprimer_Click(object sender, EventArgs e)
+        {
+            if (position == -1)
+            {
+                MessageBox.Show("Selectionnez un ligne");
+                return;
+            }
+                
+            using (SqlConnection cnx = new SqlConnection(connaixionstring))
+            {
+                if (MessageBox.Show("Avez Vous vraiment Supprimer l'etudiant !", "Supprimer", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                    return;
+
+                SqlCommand cmd = new SqlCommand("DELETE FROM Etudients WHERE ID = @id");
+                cmd.Connection = cnx;
+                cmd.CommandText = "DELETE FROM Etudients WHERE ID = @id";
+                cmd.Parameters.AddWithValue("@id", IDEtu);
+
+                cnx.Open();
+                cmd.ExecuteNonQuery();
+
+                this.DV.Rows.RemoveAt(position);
+
+                EmptyTb();
+                tbNom.Enabled = true;
+                tbPrenom.Enabled = true;
+                tbAge.Enabled = true;
+
+
+                MessageBox.Show("Le livre est bien supprimer");
+                cnx.Close();
+            }
+
+            position = -1;
+            Check = false;
+            
+            
+        }
+
+       
+    }
+}
